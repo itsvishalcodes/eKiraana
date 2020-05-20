990bf9254021eec8d613baefb986379df7be8c51
Vishal <tiwarivishal1694@gmail.com>
2020-05-20 16:59:45 +0530

    migrations done and flash taken out to application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+	resources :customers
+	resources :customerpersonalinfos
+	resources :dealers do
+		resources :products
+	end
+	resources :dealerpersonalinfos
+
+	root to: "products#index"
+
+	get "/login" => "session#new", as: "login"
+	delete "/logout" => "session#destroy", as: "logout"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/schema.rb
@@ -0,0 +1,63 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_05_20_110959) do
+
+  create_table "customerpersonalinfos", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.string "address"
+    t.integer "customer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_customerpersonalinfos_on_customer_id"
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "dealerpersonalinfos", force: :cascade do |t|
+    t.string "shopname"
+    t.integer "rating"
+    t.integer "phone"
+    t.string "review"
+    t.integer "dealer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["dealer_id"], name: "index_dealerpersonalinfos_on_dealer_id"
+  end
+
+  create_table "dealers", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "typeofproduct"
+    t.string "tags"
+    t.integer "dealer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["dealer_id"], name: "index_products_on_dealer_id"
+  end
+
+  add_foreign_key "customerpersonalinfos", "customers"
+  add_foreign_key "dealerpersonalinfos", "dealers"
+  add_foreign_key "products", "dealers"
+end
