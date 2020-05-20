a4b0e2952147ff6bf352afa32f8d0702fe39f79a
Vishal <tiwarivishal1694@gmail.com>
2020-05-20 17:46:08 +0530

    Login Controller and View Done

--- app/controllers/customersession_controller.rb
+++ app/controllers/customersession_controller.rb
@@ -4,8 +4,19 @@ class CustomersessionController < ApplicationController
   end
 
   def create
+  	current_user = Customer.find_by(email: params[:customer][:email])
+  	password = params[:customer][:password]
+
+  	if current_user && current_user.authenticate(password)
+  		session[:customer_id] = current_user.id
+  		redirect_to root_path, notice: "Logged In"
+  	else 
+  		redirect_to login_path, alert: "Oops! Wrong Credentials"
+  	end
   end
 
   def destroy
+  	reset session
+  	redirect_to login_path, notice: "You have been logged out"
   end
 end
