658df0992fdd5393e5cb7f60a36f7e29a687e737
Vishal <tiwarivishal1694@gmail.com>
2020-05-20 16:34:25 +0530

    bcrypt added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+	protect_from_forgery with: :exception
+
+	before_action :ensure_login
+	helper_method :logged_in?, :current_user
+
+	protected
+	def ensure_login
+		redirect_to login_path unless session[:customer_id]
+	end
+
+	def logged_in?
+		session[:customer_id]
+	end
+
+	def current_user
+		@current_user ||= Customer.find(session[:customer_id])
+	end
 end
