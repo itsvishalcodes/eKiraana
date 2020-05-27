9bae31851bf98ae0019a79c739751d33c6259a67
Vishal <tiwarivishal1694@gmail.com>
2020-05-27 08:50:31 +0530

    Model for product recreated

--- db/migrate/20200527031828_create_products.rb
+++ db/migrate/20200527031828_create_products.rb
@@ -2,8 +2,9 @@ class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
       t.string :name
-      t.string :typeofproduct
-      t.string :tags
+      t.float :price
+      t.text :tags
+      t.text :description
       t.references :dealer, null: false, foreign_key: true
 
       t.timestamps
