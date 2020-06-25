605672bc0a8a56365c65d22fd67410b7c0a3b0f9
Vishal <tiwarivishal1694@gmail.com>
2020-06-25 15:48:24 +0530

    Revert "Removed Rating And Review from Dealerpersonalinfo"
    This reverts commit 6ce1f3e384473c92e1c91478d459162db5eee881.

--- db/migrate/20200625074931_remove_rating_from_dealerpersonalinfo.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRatingFromDealerpersonalinfo < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :dealerpersonalinfos, :Rating, :integer
-  end
-end
--- db/migrate/20200625075015_remove_review_from_dealerpersonalinfo.rb
+++
@@ -1,5 +0,0 @@
-class RemoveReviewFromDealerpersonalinfo < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :dealerpersonalinfos, :Review, :string
-  end
-end
