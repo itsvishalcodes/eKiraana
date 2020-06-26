402f622dc01d3cad6c3d19bff125996992948f7e
Vishal <tiwarivishal1694@gmail.com>
2020-06-26 17:04:32 +0530

    Made few changes from a blog

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,14 +1,10 @@
-require 'carrierwave/storage/fog'
-
 CarrierWave.configure do |config|
-	config.storage :fog
 	config.fog_provider = 'fog/aws'
 
 	config.fog_credentials = {
 		:provider				=> "AWS",
 		:aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
 		:aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-		:region                 => 'us-east-2'
 	}
 
 	config.fog_directory	= ENV['AWS_BUCKET']
