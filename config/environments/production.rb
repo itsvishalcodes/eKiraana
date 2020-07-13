7dc21ba6b443a1366734c05c2e4d2f5f248d1d24
Vishal <tiwarivishal1694@gmail.com>
2020-07-13 09:46:21 +0530

    Static Files Cache fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,11 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+
+  config.public_file_server.headers = {
+   'Cache-Control' => "public, s-maxage=#{365.days.to_i}, maxage=#{180.days.to_i}",
+   'Expires' => "#{1.year.from_now.to_formatted_s(:rfc822)}"
+  }
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
